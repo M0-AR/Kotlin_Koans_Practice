ba6c7d579e17036877b9d8b48d2914d99a925ffd
m0-ar <s176492@student.dtu.dk>
2021-09-16 11:35:45 +0200

    Collection -> Max min.

--- Collections/Max min/src/Task.kt
+++ Collections/Max min/src/Task.kt
@@ -1,7 +1,15 @@
 // Return a customer who has placed the maximum amount of orders
-fun Shop.getCustomerWithMaxOrders(): Customer? =
-        TODO()
+fun Shop.getCustomerWithMaxOrders(): Customer? {
+        val maxLengthOfOrder = customers.map{ it.orders.size }.max()
+        return customers.first { it.orders.size == maxLengthOfOrder }
+}
 
 // Return the most expensive product that has been ordered by the given customer
-fun getMostExpensiveProductBy(customer: Customer): Product? =
-        TODO()
+fun getMostExpensiveProductBy(customer: Customer): Product? {
+        val mostExpensiveProduct = customer.orders.map{ it.products.map{ it.price }.max() }
+        val item = customer.orders.map { it.products.maxBy { it.price } }
+        val maxProductPrice = item.map { it!!.price }.max()
+        val product = item.filter { it!!.price == maxProductPrice }
+        return product.first()
+}
+

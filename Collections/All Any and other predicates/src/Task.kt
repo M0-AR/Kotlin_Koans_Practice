df51e474441be5d9fe58d60c8d73200f1cbd61da
m0-ar <s176492@student.dtu.dk>
2021-09-16 10:36:56 +0200

    Collection -> All, Any, and other predicates.

--- Collections/All Any and other predicates/src/Task.kt
+++ Collections/All Any and other predicates/src/Task.kt
@@ -1,15 +1,22 @@
 // Return true if all customers are from a given city
 fun Shop.checkAllCustomersAreFrom(city: City): Boolean =
-        TODO()
+        customers.all { it.city == city }
 
 // Return true if there is at least one customer from a given city
 fun Shop.hasCustomerFrom(city: City): Boolean =
-        TODO()
+        customers.any { it.city == city }
 
 // Return the number of customers from a given city
 fun Shop.countCustomersFrom(city: City): Int =
-        TODO()
+        customers.filter { it.city == city }.count()
 
 // Return a customer who lives in a given city, or null if there is none
-fun Shop.findCustomerFrom(city: City): Customer? =
-        TODO()
+fun Shop.findCustomerFrom(city: City): Customer? {
+        val filterCustomers = customers.filter { it.city == city }
+        return if (filterCustomers.isEmpty()) {
+                null
+        } else {
+                filterCustomers.first()
+        }
+}
+

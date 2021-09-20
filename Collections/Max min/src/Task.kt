5acfcac216ed2e84d2585e1551fdf5b6a6f8c302
m0-ar <s176492@student.dtu.dk>
2021-09-20 21:32:15 +0200

    Collection -> Max min (Suggestion solution).

--- Collections/Max min/src/Task.kt
+++ Collections/Max min/src/Task.kt
@@ -1,15 +1,10 @@
 // Return a customer who has placed the maximum amount of orders
-fun Shop.getCustomerWithMaxOrders(): Customer? {
-        val maxLengthOfOrder = customers.map{ it.orders.size }.max()
-        return customers.first { it.orders.size == maxLengthOfOrder }
-}
+fun Shop.getCustomerWithMaxOrders(): Customer? =
+       customers.maxBy { it.orders.size }
+
 
 // Return the most expensive product that has been ordered by the given customer
-fun getMostExpensiveProductBy(customer: Customer): Product? {
-        val mostExpensiveProduct = customer.orders.map{ it.products.map{ it.price }.max() }
-        val item = customer.orders.map { it.products.maxBy { it.price } }
-        val maxProductPrice = item.map { it!!.price }.max()
-        val product = item.filter { it!!.price == maxProductPrice }
-        return product.first()
-}
-
+fun getMostExpensiveProductBy(customer: Customer): Product? =
+       customer.orders
+               .flatMap(Order::products)
+               .maxBy(Product::price)

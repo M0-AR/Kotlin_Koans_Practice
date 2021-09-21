7e475abde2a9997ecbeb053375591d86810b1d72
m0-ar <s176492@student.dtu.dk>
2021-09-21 21:33:44 +0200

    Collection -> Associate (Suggestion solution).

--- Collections/Associate/src/Task.kt
+++ Collections/Associate/src/Task.kt
@@ -1,10 +1,10 @@
 // Build a map from the customer name to the customer
 fun Shop.nameToCustomerMap(): Map<String, Customer> =
-        customers.associateBy(keySelector = { it.name }, valueTransform = { it } )
+        customers.associateBy(Customer::name)
 
 // Build a map from the customer to their city
 fun Shop.customerToCityMap(): Map<Customer, City> =
-        customers.associateWith { it.city }
+        customers.associateWith(Customer::city)
 
 // Build a map from the customer name to their city
 fun Shop.customerNameToCityMap(): Map<String, City> =

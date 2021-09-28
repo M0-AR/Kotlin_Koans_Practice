a1e8ceb51f38c79733a122a121cd0fba18332c06
m0-ar <s176492@student.dtu.dk>
2021-09-28 21:41:02 +0200

    Collection -> Compound ( Suggestion solution).

--- Collections/Compound tasks/src/Task.kt
+++ Collections/Compound tasks/src/Task.kt
@@ -1,18 +1,20 @@
 // Find the most expensive product among all the delivered products
 // ordered by the customer. Use `Order.isDelivered` flag.
 fun findMostExpensiveProductBy(customer: Customer): Product? {
-     val orders = customer.orders.filter { it.isDelivered }
-     val flatMap = orders.flatMap { it.products }
-    return flatMap.maxBy { it.price }
+    return customer
+            .orders
+            .filter(Order::isDelivered)
+            .flatMap(Order::products)
+            .maxBy(Product::price)
 }
 
 // Count the amount of times a product was ordered.
 // Note that a customer may order the same product several times.
 fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
     return customers
-                .flatMap(Customer::getOrderedProducts)
-                .count { it == product }
+            .flatMap(Customer::getOrderedProducts)
+            .count { it == product }
 }
 
 fun Customer.getOrderedProducts(): List<Product> =
-            orders.flatMap(Order::products)
+        orders.flatMap(Order::products)
